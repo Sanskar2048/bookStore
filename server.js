33f9e43d7327f565db83c64735843c2efdfc9292
sanskar2048 <ss9814@srmist.edu.in>
2021-09-18 14:37:22 +0530

    setup the mongodb

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').parse()
+    require('dotenv').config()
 }
 
 const express = require('express')
-const mongoose=require('mongoose')
 const app = express()
 const expressLayouts = require('express-ejs-layouts');
 
@@ -15,6 +14,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+const mongoose=require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true })
 const db = mongoose.connection
